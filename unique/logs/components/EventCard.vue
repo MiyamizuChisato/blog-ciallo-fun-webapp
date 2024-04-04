b7f5284f3908ca2dd3e737fb8f30400f65c0c1a1
MiyamizuChisato <miyamizuchisato@gmail.com>
2024-04-04 13:46:50 +0800

    20240404

---
+++ unique/logs/components/EventCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  version: string
+  date: string
+  content: string[]
+}>()
+</script>
+
+<template>
+  <div data-aos="fade-up" class="text-stress text-xl font-bold">
+    {{ version }}
+  </div>
+  <div data-aos="fade-up" class="text-muted">
+    {{ date }}
+  </div>
+  <div data-aos="fade-up" class="text-holder pt-2">
+    <div v-for="i in content">{{ i }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
